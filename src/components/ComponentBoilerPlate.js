58ef51779075205588ebf3e6082e392e1dc26bd3
Alex Philayvanh <aphilay@gmail.com>
2019-09-29 21:39:41 -0700

    initial commit

---
+++ src/components/ComponentBoilerPlate.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const componentName = () => {
+  return (
+    <View>
+      <Text>componentName</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default componentName;
